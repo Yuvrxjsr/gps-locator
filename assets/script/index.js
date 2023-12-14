c18637d83f1c00dab2deea563e7391d4415d1f42
Yuvraj Singh <yuvrajsinghrandhawaishere@gmail.com>
2023-12-14 14:06:49 -0600

    fix js

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,48 +1,80 @@
-'use strict';
+"use strict";
 
-function onEvent(event, selector, callback) {
-    return selector.addEventListener(event, callback);
+let lat, long;
+
+mapboxgl.accessToken =
+  "pk.eyJ1IjoieXV2cnhqc3IiLCJhIjoiY2xxM3g4a3NhMDE0bzJrbnZ6dGp6cmQwYSJ9.ko0Ddi2P09rnxx5TYkiSpQ";
+const map = new mapboxgl.Map({
+  container: "map", // container ID
+  center: [-97.143173, 49.895313], // starting position [lng, lat]
+  zoom: 4, // starting zoom
+  pitch: 40,
+  style: "mapbox://styles/mapbox/streets-v12", // style URL
+});
+
+map.dragPan.disable();
+map.keyboard.disable();
+map.scrollZoom.disable();
+map.doubleClickZoom.disable();
+// map.touchZoomRotate.disableRotation();
+
+function getLocation(position) {
+  const { latitude, longitude } = position.coords;
+
+  lat = latitude;
+  long = longitude;
+  // Center the map on the user's position
+  goCenter(latitude, longitude);
+  // Create a marker of the user's position on the map
+  createMarker(latitude, longitude);
 }
 
-
-function getLocation(position){    
-    const {latitude, longitude} = position.coords;
-
-    mapConfig(longitude, latitude);
+// Center the map on the coordinates of the user's position
+function goCenter(latitude, longitude) {
+  map.flyTo({
+    center: [longitude, latitude],
+    essential: true, // this animation is considered essential with respect to prefers-reduced-motion
+    zoom: 16,
+  });
 }
 
-function errorHandler(){
-    mapConfig(-97.19267,49.81486);
-}
+// Store the current marker
+let currentMarker = null;
 
-const options = {
-    enableHighAccuracy: true,
-    maximumAge: 0
-}
+// Create a marker on the map
+function createMarker(latitude, longitude) {
+  // If a marker already exists, remove it
+  if (currentMarker) {
+    currentMarker.remove();
+  }
 
-mapboxgl.accessToken = 'pk.eyJ1IjoieXV2cnhqc3IiLCJhIjoiY2xxM3g4a3NhMDE0bzJrbnZ6dGp6cmQwYSJ9.ko0Ddi2P09rnxx5TYkiSpQ';
-
-function mapConfig(longitude, latitude){
-    const map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v12',
-        center: [longitude,latitude],
-        zoom: 16
-    });
-
-    map.addControl(
-        new MapboxDirections({
-        accessToken: mapboxgl.accessToken
-        }),
-        'top-left'
-    );
-    const marker1 = new mapboxgl.Marker({color: '#ff7342'})
+  // Create a default Marker and add it to the map.
+  const marker = new mapboxgl.Marker({
+    color: "#0599ff",
+    rotation: 0,
+  })
     .setLngLat([longitude, latitude])
     .addTo(map);
+
+  // Update the current marker
+  currentMarker = marker;
 }
 
-onEvent('load', window, () => {
-    if(navigator.geolocation) {
-        navigator.geolocation.watchPosition(getLocation, errorHandler, options);
-    }
-});
+// The error/failure callback function
+function errorHandler(error) {
+  console.log(error.message);
+}
+
+// Location options
+const options = {
+  maximumAge: 0, // don't use the cached location
+  timeout: 5000,
+  enableHighAccuracy: true,
+};
+
+if ("geolocation" in navigator) {
+  const geo = navigator.geolocation;
+  geo.watchPosition(getLocation, errorHandler, options);
+} else {
+  alert("Geolocation API is not supported by your browser");
+}
